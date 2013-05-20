a603c8d51b0fb595b2f17f94e42c8a9f5022c12a
Rohit Chatterjee <RohitC@microland.com>
2013-05-20 16:03:56 +0530

    first commit of split_file

---
+++ split_file.py
@@ -0,0 +1,30 @@
+# @brief Split a file randomly into two components
+# @author rohit
+import sys
+import argparse
+import random
+
+parser = argparse.ArgumentParser ("Split a file randomly into two components")
+parser.add_argument("--input", help="input file", required=True)
+parser.add_argument("--output1", help="output file 1", required=True)
+parser.add_argument("--output2", help="output file 2", required=True)
+parser.add_argument("--ratio", required=True, type=float, help="split ratio")
+args = parser.parse_args()
+
+if (args.ratio <= 0 or args.ratio >= 1):
+    print ("ratio needs to lie in the open interval (0,1)")
+    sys.exit(1)
+
+with open(args.input, "r") as inputfile:
+    header = inputfile.readline()
+    outputfile1 = open (args.output1, "w")
+    outputfile2 = open (args.output2, "w")
+    outputfile1.write(header)
+    outputfile2.write(header)
+    for line in inputfile.readlines():
+        rand = random.random()
+        if (rand <= args.ratio):
+            outputfile1.write(line)
+        else:
+            outputfile2.write(line)
+                
