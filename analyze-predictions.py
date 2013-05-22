354ebc19500767f0ce3266b4300704124745a355
Rohit Chatterjee <RohitC@microland.com>
2013-05-22 14:46:46 +0530

    pred-steps and top-n

--- analyze-predictions.py
+++ analyze-predictions.py
@@ -4,102 +4,63 @@ import sys
 import argparse
 from math import sqrt
 
-def show_stats (metric, inputfilename, show_vals):
+def show_stats (metrics, metric, inputfilename, show_vals, pred_step, top_n):
     mse = 0
     total = 0
     count = 0
     with open(inputfilename, "r") as inputfile:
-        headers = inputfile.readline().strip().split()
-        metric_idx = headers.index(metric)
+        metric_idx = metrics.index(metric)
+        inputfile.readline() # discard header
+        npred = 0
+        if show_vals:
+            print("%10s\t%8s\t%8s\t%8s\t%8s" % ("metric", "actual", "pred", "error", "error%"))
+            print ("=" * 80)
         for line in inputfile.readlines():
             c = line.strip().split()
-            actual_val = float(c[0])
-            pred_val = float(c[metric_idx])
+            if npred==0:
+                npred = int(len(c) / (len(metrics) + 1))
+                #print ("len(c)=%d len(metrics)=%d npred=%d" % (len(c), len(metrics), npred))
+                if pred_step >= npred:
+                    raise IndexError("pred_step must be <= %d" % npred)
+            actual_val = float(c[pred_step])
+            pred_val = float(c[npred + metric_idx + len(metrics) * pred_step])
             error_val = actual_val - pred_val
             mse += (error_val * error_val)
             count += 1
             total += actual_val
             if show_vals:
-                error_pctg = error_val / actual_val * 100
+                error_pctg = error_val / actual_val * 100 if actual_val != 0 else 0
                 print ("%10s\t%02.5f\t%02.5f\t%+02.5f\t%+03.2f" % (metric, actual_val, pred_val, error_val, error_pctg))
-              
+            if top_n > 0 and count == top_n:
+                break
+        if count == 0:
+            print ("no data")
+            return
         mse /= count
-        total /= count
+        total = abs(total/count)
         rmse = sqrt (mse)
-        print ("RMSE for %10s is %3.5f on %5.5f (%05.5f %%)" % (metric, rmse, total, rmse/total*100))
+        if show_vals:
+            print ("=" * 80)
+        print ("RMSE for %10s, %d steps ahead, is %3.5f on a mean of %5.5f" % (metric, pred_step + 1, rmse, total))
 
 def get_metrics (inputfilename):
     with open(inputfilename, "r") as inputfile:
         header = inputfile.readline().strip()
-        return header.split()
+        return header.split()[1:] # discard "actual" (which is always there)
 
 parser = argparse.ArgumentParser("Prediction analysis")
 parser.add_argument("--input", required=True)
 parser.add_argument("--metric", required=True, help="name of metric, or list-all, or all")
+parser.add_argument("--pred-step", default=1, type=int, help="number of steps ahead to predict (that step only)")
 parser.add_argument("--show-vals", action="store_true")
+parser.add_argument("--top-n", type=int, metavar="N", default=0, help="process only the top N rows")
 args = parser.parse_args()
 
+metrics = get_metrics(args.input)
 if args.metric == "list-all":
-    print (", ".join(get_metrics(args.input)))
+    print (", ".join(metrics))
 elif args.metric == "all":
-    for metric in get_metrics(args.input):
-        show_stats (metric, args.input, args.show_vals)
+    for metric in metrics:
+        show_stats (metrics, metric, args.input, args.show_vals, args.pred_step - 1, args.top_n)
 else:
-    show_stats (args.metric, args.input, args.show_vals)
-        
-
-#
-def display_stats (error_seq):
-    nreadings = len(error_seq[error_seq.keys()[0]])
-    display_stats_upto(error_seq, nreadings/4*1)
-    display_stats_upto(error_seq, nreadings/4*2)
-    display_stats_upto(error_seq, nreadings/4*3)
-    display_stats_upto(error_seq, nreadings/4*4)
-
-def display_stats_upto(error_seq, nreadings):
-    metrics = error_seq.keys()
-
-    errors = dict()
-    best_worst = dict()
-
-    for reading_idx in range(0,nreadings):
-        for metric in metrics:
-            errors[metric] = error_seq[metric][reading_idx]
-
-            
-##            for (int reading_idx = 0; reading_idx < nreadings; ++reading_idx)
-##            {
-##                foreach (string metric in metrics) errors[metric] = error_seq[metric][reading_idx];
-##                // best and worsts
-##                var sorted = errors.OrderBy(a => Math.Abs(a.Value));
-##                string best_metric = sorted.ElementAt(0).Key;
-##                string worst_metric = metrics.Contains("MvAvg") ? sorted.ElementAt(metrics.Count() - 2).Key : sorted.ElementAt(metrics.Count() - 1).Key;
-##                Tuple<int, double, int, double> best = best_worst_metric[best_metric];
-##                Tuple<int, double, int, double> worst = best_worst_metric[worst_metric];
-##                best_worst_metric[best_metric] = System.Tuple.Create(best.Item1 + 1, best.Item2 + Math.Abs(errors[best_metric]), best.Item3, best.Item4);
-##                best_worst_metric[worst_metric] = System.Tuple.Create(worst.Item1, worst.Item2, worst.Item3 + 1, worst.Item4 + Math.Abs(errors[worst_metric]));
-##
-##            }
-##            Console.WriteLine("Using {0} readings:", nreadings);
-##            Console.WriteLine("== Best & Worst==");
-##            Console.WriteLine("{0,20} {1,14} {2,15} {3,14} {4,15} {5,15}", "Metric", "Freq (Best)", "Abs-Err (Best)", "Freq (Worst)", "Abs-Err (Worst)", "Freq (Best-Worst)");
-##            foreach (var el in best_worst_metric.OrderByDescending(a => a.Value.Item1))
-##            {
-##                Console.WriteLine("{0,20} {1,14} {2,15:F5} {3,14} {4,15:F5} {5,15}", el.Key,
-##                    best_worst_metric[el.Key].Item1, best_worst_metric[el.Key].Item2 / best_worst_metric[el.Key].Item1,
-##                    best_worst_metric[el.Key].Item3, best_worst_metric[el.Key].Item4 / best_worst_metric[el.Key].Item3,
-##                    best_worst_metric[el.Key].Item1 - best_worst_metric[el.Key].Item3);
-##            }
-##            // R-statistics
-##            Dictionary<string, double> sum_abs_error = new Dictionary<string, double>();
-##            foreach (string metric in metrics)
-##            {
-##                sum_abs_error[metric] = error_seq[metric].Take(nreadings).Select(e => Math.Abs(e)).Sum();
-##            }
-##            double total_sum = sum_abs_error.Sum(a => a.Value);
-##            Console.WriteLine("== R-score ==");
-##            foreach (var el in sum_abs_error.OrderBy(a => a.Value))
-##            {
-##                Console.WriteLine("{0,20} {1:F5}", el.Key, el.Value / total_sum);
-##            }
-##        }
+    show_stats (metrics, args.metric, args.input, args.show_vals, args.pred_step - 1, args.top_n)
