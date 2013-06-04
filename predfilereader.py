8e03b2a9d2e96fe1daae3ccedf9dba4c45b83b22
Rohit Chatterjee <RohitC@microland.com>
2013-06-04 16:59:12 +0530

    took out the metric_idx from the object

--- predfilereader.py
+++ predfilereader.py
@@ -4,24 +4,23 @@ import os
 import sys
 # ==========================================================================================================        
 class PredFileReader:
-    def __init__(self, inputfilename, metric_idx):
+    def __init__(self, inputfilename):
         self.inputfile = open(inputfilename, "r")
         self.versnum = 0
         self.npred = None
         self.metrics = []
-        if metric_idx < 0: raise ValueError
-        self.metric_idx = metric_idx
         self.first_time = True
         
-    def get_actual(self, entries, pred_step):
-        return float(entries[pred_step])
+    def get_actual(self, entries, step):
+        return float(entries[step])
             
-    def get_predicted(self, entries, pred_step):
+    def get_predicted(self, entries, step, metric_idx):
+        if (1 + metric_idx) >= len(self.metrics): raise ValueError
         if self.versnum == 0:
-            pred_val = float(entries[self.npred + self.metric_idx + len(self.metrics) * pred_step])
+            pred_val = float(entries[self.npred + metric_idx + len(self.metrics) * step])
             return pred_val
         elif self.versnum == 1:
-            pred_val = float(entries[pred_step + self.npred * (1 + self.metric_idx)])
+            pred_val = float(entries[step + self.npred * (1 + metric_idx)])
             return pred_val
         else:
             print ("cannot handle version %d, quitting." % self.versnum)
@@ -37,11 +36,10 @@ class PredFileReader:
                 self.metrics = self.inputfile.readline().strip().split()  # including "actual"
             else:
                 self.metrics = line
-            if (1 + self.metric_idx) >= len(self.metrics): raise ValueError
             values = self.inputfile.readline().strip().split()
             if self.versnum == 0:
                 self.npred = int(len(values) / len(self.metrics))
-            if self.npred < 0:
+            if self.npred <= 0:
                 raise InputError
             self.first_time = False
             return values
